abd1cb16db2591f7f2dc74238351c9ba043e6888
carstenmontag <carsten.montag@companymind.de>
2022-01-16 19:41:37 +0100

    calc stats logic

--- src/main/java/ludo/CSVHandler.java
+++ src/main/java/ludo/CSVHandler.java
@@ -8,11 +8,8 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class CSVHandler {
-    GameSave[] games_this_comb;
+    ArrayList<GameSave> games_this_comb = new ArrayList<GameSave>();
 
-    public CSVHandler(int games_per_comb){
-        this.games_this_comb = new GameSave[games_per_comb];
-    }
     public void writeRowToCSV(String[] row, String filepath){
         try{
             FileWriter fw = new FileWriter(filepath,true);
@@ -57,7 +54,52 @@ public class CSVHandler {
         return result;
     }
     public void add_comb(){
+        // TODO null Turns werden noch nicht gezählt
+        // stats to calculate
+        int games = games_this_comb.size();
+        double win_rate;
+        double turns_to_finish;
+        double average_placement;
+        double average_turns_per_game;
+        double average_blocks_created;
+        double average_kicks;
+        double average_got_kicked;
+        int game_most_kicks = 0;
+        int game_most_blocks = 0;
+        int game_most_got_kicked = 0;
+
+        // helper variables
+        int turns_observed_total = 0;
+        int sum_placement = 0;
+        int games_won=0;
+        int game_durations_sum = 0;
+        int blocks_total = 0;
+        int got_kicked_total = 0;
+        int kicked_total = 0;
+
+        Iterator<GameSave> it = games_this_comb.iterator();
+        // iteration
+        while(it.hasNext()){
+            GameSave currentGame = (GameSave)it.next();
+            sum_placement += currentGame.placement;
+            game_durations_sum += currentGame.game_duration;
+            turns_observed_total += currentGame.number_of_moves;
+            blocks_total += currentGame.number_of_blocks;
+            got_kicked_total += currentGame.number_has_been_kicked;
+            kicked_total += currentGame.number_has_been_kicked;
+            if (currentGame.placement == 1){games_won++;}
+            if (game_most_blocks<currentGame.number_of_blocks){game_most_blocks = currentGame.number_of_blocks;}
+            if (game_most_kicks<currentGame.number_of_kicks){game_most_kicks = currentGame.number_of_kicks;}
+            if (game_most_got_kicked<currentGame.number_has_been_kicked){game_most_got_kicked = currentGame.number_has_been_kicked;}
+        }
         
+        win_rate = games_won/games;
+        average_placement = sum_placement/games;
+        average_turns_per_game = game_durations_sum/games;
+        turns_to_finish = turns_observed_total/games;
+        average_blocks_created = blocks_total/games;
+        average_kicks = kicked_total/games;
+        average_got_kicked = got_kicked_total/games;
     }
     public void add_run(ArrayList<Move> current_game, int current_index, int placement ){
         int number_of_blocks = 0;
@@ -79,7 +121,7 @@ public class CSVHandler {
             }
         }
         System.out.println("Current Index :" + current_index);
-        games_this_comb[current_index-1]= new GameSave(number_of_blocks,number_of_kicks,number_of_moves,number_has_been_kicked,placement,game_duration);
+        games_this_comb.add(new GameSave(number_of_blocks,number_of_kicks,number_of_moves,number_has_been_kicked,placement+1,game_duration));
     }
 
 }
