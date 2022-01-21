8eb084dacc2eee964efb5c6b5cf28f0702507140
carstenmontag <carsten.montag@companymind.de>
2022-01-21 23:24:17 +0100

    Javadoc Player

--- src/main/java/ludo/Player.java
+++ src/main/java/ludo/Player.java
@@ -10,10 +10,13 @@ import java.util.Arrays;
 
 import ec.util.*;
 
+
+
+/**
+ * @className Player
+ * @description Das Objekt Player imlementiert das Interface Steppable und ist somit ein Agent bzw Akteur in der Simulationsumgebung
+ */
 public class Player implements Steppable {
-    //Agent, jeder Agent soll 4 Spielfiguren haben (sind die Spielfiguren Objekte?)
-    //Step, Würfeln, Figur bewegen, falls schon eine Figur da ist Entscheidung treffen, Position der Figur aktualisieren und repeat
-    public int diceNumber;
     public String name;
     public String strategy;
     public Int2D[] two_d_spawns;
@@ -21,20 +24,21 @@ public class Player implements Steppable {
     public int start;
     public int finish;
     public int playerIndex;
-    public int order;
     public MersenneTwisterFast randomGenerator;
     public GamePiece[] AtStartPieces = new GamePiece[4];
     public SparseGrid2D tempBoard;
-    public SparseGrid2D finishLine;
     public int placement;
-    //public int figureNumber = 1;
 
-    //Die Zielline, SparseGrid2D damit mehrere Objekte (Spielfiguren?) darauf gespeichert werden können.
-    //Die Ziellinie ist eine eigene Entität für den jeweiligen Spieler, die Verwendung in der Simulation ist unnötig
-    
-    public int finishLineWidth = 4;
-    public int finishLineHeight = 1;
-
+    /**
+     *
+     * @param name      Spielername, wichtig wenn "Observed"
+     * @param strategy  Strategie des Spielers, implementiert durch einen switch case, 
+     *                  welcher je nach Taktik mithilfe von custom Comparators einen Zug aussucht
+     * @param rng       MersenneTwisterFast
+     * @param tempBoard Referenz auf ddas Spielfeld 
+     * @return Player 
+     
+     */
     public Player(String name, String strategy, MersenneTwisterFast rng, SparseGrid2D tempBoard){
         super();
         this.name = name;
@@ -42,11 +46,18 @@ public class Player implements Steppable {
         this.randomGenerator = rng;
         this.tempBoard = tempBoard;
         this.placement = 0; //1=1Platz, 2=2Platz ...
-        
     }
+    /** 
+     * @description Die step Funktion des Agenten. In ihr würfelt jeder Agent, auf Basis dieses Wurfes werden 4 Move Objekte erstellt.
+     *              Diese werden vom Player anhand der Strategie mithilfe eines Comparators sortiert. 
+     *              Der beste Move wird ausgewählt.
+     * @param state Der aktuelle Status der Simulationsumgebung
+     */
     public void step(SimState state){
         System.out.println("Turn Player " + playerIndex);
+        // cast für Methoden und Variablenzugriff
         PlayingGround gameboard = (PlayingGround)state;
+        // Das aktuelle 2D Spielfeld 
         tempBoard = gameboard.field;
         //Mit state bekommt der Agent den aktuellen Status
         //Hier sollen die Aktionen durchgeführt werden
@@ -81,17 +92,23 @@ public class Player implements Steppable {
         }
     }
 
-    // mögliche Züge werden in der Form eines Objektes dargestellt
-    // für jeden Move wird errechnet ob er durch die Spielrestriktionen mgl ist
-    // sind mehrere Moves mgl so wird nach der angegebenen Strategie entschieden --> ein Move bekommt Attribute wie "beat" oder "keep up", hiernach kann entschieden werden
+    
+    /** 
+     * @description         Diese Funktion erstellt auf Basis des Wurfes 4 Moveobjekte, diese werden in ihrem Konstruktor
+     *                      auf Spielrestriktionen geprüft und bei Verletzung aussortiert,
+     * @param eyesThisMove  Zahl zwischen 1 und 6, generiert vom MersenneTwisterFast
+     * @return Move[]       Das Array mit allen möglichen Moves.
+     */
     public Move[] getPossibleMoves(int eyesThisMove){
         int possible_counter = 0;
         Move[] moves = new Move[4];
         for(int i = 0; i<AtStartPieces.length; i++){
             Move move = new Move(AtStartPieces[i], eyesThisMove, AtStartPieces, name, tempBoard);
-            if (move.possible){possible_counter++;}
+            // Konstruktor überprüft auf Spielregeln, Blocks etc.
+            if (move.possible){possible_counter++;} // Größe für das Array mgl Moves wird bei Iteration ermittelt.
             moves[i] = move;
         }
+        // Mögliche Moves werden zurück gegeben
         Move[] possibleMoves = new Move[possible_counter];
         int nextIndex = 0;
         for(int i = 0;i<moves.length;i++){
@@ -103,8 +120,13 @@ public class Player implements Steppable {
         return possibleMoves;
     }
 
+    
+    /** 
+     * @description Ein Comparator basierend auf dem String strategy wird genutzt um die Moves zu sortieren. Der beste wird ausgewählt
+     * @param moves Das Array mit allen möglichen Spielzügen diesen Zug.
+     * @return Move Der preferierte Move nach der zugewiesenen Strategie.
+     */
     public Move determineMove(Move[] moves){
-        //Strategien implementieren
         // random decision default
         // implemented tactics are executed in switch case statement
         ArrayList<Move> movesArrayList = new ArrayList<Move>(Arrays.asList(moves));
@@ -132,6 +154,14 @@ public class Player implements Steppable {
         }
     }
 
+    
+    /** 
+     * @param start         Startpunkt auf dem Spielfeld 1D
+     * @param finish        letzter Platz vor dem Ziel
+     * @param spawns        Startfelder der Spielsteine 2D
+     * @param finish_line   die 2D Felder, welche die Zielline für den Spieler darstellen.
+     * @param playerIndex   index im Array players der aktuellen PlayingGround Instanz
+     */
     public void setOrderDependantVariables(int start, int finish, Int2D[] spawns, Int2D[] finish_line, int playerIndex){
         this.start = start;
         this.finish = finish;
@@ -141,16 +171,20 @@ public class Player implements Steppable {
         System.out.println("Variables for Player "+name+" set.");
         createFigures();    
     }
-
-    public void createFigures(){
-        //Die Figuren sollen auf pro Spieler Basis existieren, ist das möglich? Vielleicht die Figuren als einfache Attribute
-        //Oder die Figuren als Objekte in der Simulationsenvironment, wäre vermutlich sinnvoll
+    /**
+    * @description Spielfiguren werden erstellt und auf das entsprechende Startfeld aus dem spawns Array gesetzt
+    */
+     public void createFigures(){
         for(int i = 0;i<=AtStartPieces.length-1;i++){
             AtStartPieces[i] = new GamePiece(playerIndex,start,finish,i, two_d_spawns[i],two_d_finish_line,name,tempBoard);
             AtStartPieces[i].set_to_spawn();
         }
     }
 
+    
+    /** 
+     * @return int
+     */
     public int throwDice(){
         //Gibt eine Zahl zwischen 1 und 6 für den Würfelwurf aus, besser als Javas Random 
         return randomGenerator.nextInt(6)+1;
